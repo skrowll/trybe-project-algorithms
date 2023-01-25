4607a9a8e6f9972503cef5bbbb7272b579c3498e
skrowll <lucasdejesus.rocha@hotmail.com>
2023-01-25 19:00:09 -0300

    feat(Req. 03): create function is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+    if high_index <= low_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
