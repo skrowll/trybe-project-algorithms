bea4915e1a8f15099be316037bbdac13ad4ea671
skrowll <lucasdejesus.rocha@hotmail.com>
2023-01-25 18:42:11 -0300

    feat(Req. 02): create test function test_encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    messages = ["qwerty", 1]
+    keys = [3, 4, 9, "qwerty"]
+
+    assert encrypt_message(messages[0], keys[0]) == "ewq_ytr"
+    assert encrypt_message(messages[0], keys[1]) == "yt_rewq"
+    assert encrypt_message(messages[0], keys[2]) == "ytrewq"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(messages[0], keys[3])
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(messages[1], keys[0])
